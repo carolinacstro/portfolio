f3af45ed69f6e5b3247be2219d8ab3a986ab2515
Carolina Castro <carolinacoliveira@outlook.com>
2022-05-08 18:15:35 -0300

    modificacoes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 // arquivo js
 
-// function typeWriter(elemento) {
-//     const textoArray = elemento.innerHTML.split('');
-//     elemento.innerHTML = '';
-//     textoArray.forEach((letra, i) => {
-//         setTimeout(function() {
-//             elemento.innerHTML += letra; 
-//         }, 150 * i)
-//     });
-// }
-
-// const titulo = document.querySelector('h1');
-// typeWriter(titulo);
-
 function getRepos() {
     fetch('https://api.github.com/users/carolinacstro/repos', {method: 'GET'}).then(response => {
         response.json().then(data => {renderRepos(data)})
@@ -27,29 +14,9 @@ function renderRepos(repositories) {
             <div class="card">
                 <h3 class="card__title">${repo.name.replaceAll("-", " ").replaceAll("_", " ")}</h3>
                 <p class="card__description">${repo.description}</p>
-                <p> Tópicos do projeto: ${repo.topics.join(', ')}</p>
+                <p class="card__topics"> Tópicos do projeto: ${repo.topics.join(', ')}</p>
             </div>
         </a>`).join('')
 }
 
 renderRepos();
-
-// function getImages(value) {
-
-//     const languages = ;
-
-//     switch (lenguages) {
-//         case "HTML":
-//             image = '<img src="http://smallbeerpress.com/wp-content.png"></img>';
-//         case "CSS":
-//         case "Javascript":
-//         case "Ruby":
-//         case "Python":
-//         default:
-//             break
-//     }
-
-//     console.log(languages);
-// }
-
-// getImages();
